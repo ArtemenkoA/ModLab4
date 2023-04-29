8fe98b0151d55dd3ec5acd4dc3b15002a236955a
79152 <7915241@gmail.com>
2023-04-29 17:51:02 +0300

    Добавьте файлы проекта.

---
+++ ModLab3/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ModLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea13 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend13 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series37 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series38 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series39 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea14 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend14 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series40 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series41 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series42 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderSkin.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.Cross;
+            this.chart1.BorderSkin.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipX;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.Transparent;
+            chartArea13.AxisX.IsLabelAutoFit = false;
+            chartArea13.AxisX2.IsLabelAutoFit = false;
+            chartArea13.AxisY.IsLabelAutoFit = false;
+            chartArea13.AxisY2.IsLabelAutoFit = false;
+            chartArea13.Name = "Событийный принцип";
+            this.chart1.ChartAreas.Add(chartArea13);
+            legend13.Name = "Legend1";
+            this.chart1.Legends.Add(legend13);
+            this.chart1.Location = new System.Drawing.Point(338, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series37.BorderWidth = 3;
+            series37.ChartArea = "Событийный принцип";
+            series37.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series37.Legend = "Legend1";
+            series37.LegendText = "Размер очереди";
+            series37.MarkerBorderWidth = 5;
+            series37.MarkerColor = System.Drawing.Color.Red;
+            series37.Name = "Размер очереди";
+            series37.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            series37.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series38.BorderWidth = 3;
+            series38.ChartArea = "Событийный принцип";
+            series38.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series38.Legend = "Legend1";
+            series38.Name = "Количество сообщений в очереди";
+            series38.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            series38.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series39.BorderWidth = 3;
+            series39.ChartArea = "Событийный принцип";
+            series39.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series39.Legend = "Legend1";
+            series39.Name = "Количество пропущенных сообщений";
+            series39.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt32;
+            series39.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series37);
+            this.chart1.Series.Add(series38);
+            this.chart1.Series.Add(series39);
+            this.chart1.Size = new System.Drawing.Size(1048, 347);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(246, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // chart2
+            // 
+            chartArea14.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea14);
+            legend14.Name = "Legend1";
+            this.chart2.Legends.Add(legend14);
+            this.chart2.Location = new System.Drawing.Point(338, 421);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series40.BorderWidth = 3;
+            series40.ChartArea = "ChartArea1";
+            series40.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series40.LabelBorderWidth = 3;
+            series40.Legend = "Legend1";
+            series40.Name = "Размер очереди";
+            series41.BorderWidth = 3;
+            series41.ChartArea = "ChartArea1";
+            series41.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series41.LabelBorderWidth = 3;
+            series41.Legend = "Legend1";
+            series41.Name = "Количество сообщений в очереди";
+            series42.BorderWidth = 3;
+            series42.ChartArea = "ChartArea1";
+            series42.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series42.LabelBorderWidth = 3;
+            series42.Legend = "Legend1";
+            series42.Name = "Количество пропущенных сообщений";
+            this.chart2.Series.Add(series40);
+            this.chart2.Series.Add(series41);
+            this.chart2.Series.Add(series42);
+            this.chart2.Size = new System.Drawing.Size(1048, 369);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Время моделирования в секундах:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выбор  Δt:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(98, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(55, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(159, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "в милисекундах";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(196, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 33);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(100, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 33);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(691, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 22);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Событийный принцип:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(731, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Принцип Δt:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1398, 840);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Моделирование";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ModLab3/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModLab3
+{
+    public partial class Form1 : Form
+    {
+        Queue<double> queue = new Queue<double>();
+        int qLen = 10;
+
+        List<double> list = new List<double>();
+        Random rnd = new Random();
+        int count=0;
+        int count1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer2 = new System.Windows.Forms.Timer();
+            timer3 = new System.Windows.Forms.Timer();
+
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer3.Tick += new EventHandler(timer3_Tick);
+
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+            Task.Run(() =>
+            {   Stopwatch sw = new Stopwatch();
+                sw.Start();
+                while (sw.Elapsed.TotalSeconds < int.Parse(textBox1.Text)){int c = 0;c++;}
+                sw.Stop();
+                timer1.Stop();
+                timer2.Stop();
+                timer3 .Stop();
+            });
+        }
+
+        public static int getPoisson(double lambda)
+        {
+            Random rnd = new Random();
+            double L = Math.Exp(-lambda);
+            double p = 1.0;
+            int k = 0;
+            do
+            {
+                k++;
+                p *= rnd.NextDouble();
+            } while (p > L);
+            return k-1;
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = rnd.Next(300,500);
+            queue.Enqueue(rnd.NextDouble());
+            if (queue.Count > qLen)
+            {
+                qLen = queue.Count;
+            }
+            if (queue.Count != 0)
+            {
+                chart1.Series[0].Points.AddXY(count1, qLen);
+                chart1.Series[1].Points.AddXY(count1, queue.Count);
+                chart1.Series[2].Points.AddXY(count1, list.Count);
+                count++;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Interval = getPoisson(rnd.Next(400, 600));
+
+            if (queue.Count != 0)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count > 10)
+            {
+                list.Add(1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+            comboBox1.Items.AddRange(new object[] { 100, 200, 500, 1000 });
+        }
+        private void progressBar1_Click(object sender, EventArgs e){}
+        private void chart1_Click(object sender, EventArgs e) {}
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Interval = Convert.ToInt32(comboBox1.Text);
+            if (queue.Count > qLen)
+            {
+                qLen = queue.Count;
+            }
+            if (queue.Count != 0)
+            {
+                chart2.Series[0].Points.AddXY(count, qLen);
+                chart2.Series[1].Points.AddXY(count, queue.Count);
+                chart2.Series[2].Points.AddXY(count, list.Count);
+                count++;
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e){}
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            queue.Clear();
+            list.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart2.Series[2].Points.Clear();
+        }
+    }
+}
